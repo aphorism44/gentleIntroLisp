f72b1fb965548ae4a355757fdc7a63aa232b6d32
aphorism44 <dom.jesse6@gmail.com>
2019-09-01 13:56:32 -0500

    complete ch5 start ch6

---
+++ ch6exercises.lisp
@@ -0,0 +1,8 @@
+;chapter 6 exercises
+
+;6.6 - LAST=ELEMENT - a. return LAST element of list rather than last cons cell;
+; b. write version that uses REVERSE instead of LAST
+; c. write version using NTH and LENGTH
+
+(defun last-element (list)
+   )
